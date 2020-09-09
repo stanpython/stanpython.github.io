833489943230868a878e81265ea4f1c77b0e10ea
stanleyhuang <huang.stan@icloud.com>
2020-09-09 13:25:28 -0400

    fixing formatting

--- src/Apple.js
+++ src/Apple.js
@@ -1,11 +1,8 @@
 class Apple {
-
   constructor($el) {
     this.node = $('<img id="apple"></img>');
-    this.node.attr('src', './apple5.png');
+    this.node.attr("src", "./apple5.png");
     $el.append(this.node);
     this.node.css({ top: Math.round((Math.random() * 650) / 50) * 50, left: Math.round((Math.random() * 650) / 50) * 50 });
   }
-
 }
-
--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,7 @@
 class Body {
   constructor($el) {
     this.node = $('<div class="body"><div class="spots"></div></div>');
-    this.currentDirection = 'right';
+    this.currentDirection = "right";
     this.SPEED = 200;
     $el.append(this.node);
     // this.node.css({ top: 0, left: 0 });
@@ -10,9 +10,7 @@ class Body {
   }
 
   move() {
-
     this.node.css(position);
     // setTimeout(this.move.bind(this), this.SPEED);
   }
-
 }
